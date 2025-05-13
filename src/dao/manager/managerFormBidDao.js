e08b5c60fbc9038251c9240b77c7c568885c2dd3
leejhyun13 <leejh44351@naver.com>
2025-05-13 14:57:25 +0900

    feat: 장례식장 견적서 조회 및 입찰 신청 기능 추가
    - 장례식장 ID를 통해 모든 상조팀장의 견적서 리스트를 조회하는 기능 추가
    - managerFormBidId를 기반으로 견적서 상세 내용 및 입찰 신청 기능 구현
    - 관련 DAO, 서비스, 라우터 설정을 통해 API 엔드포인트 추가
    - 필수 필드 및 UUID 검증 미들웨어 적용으로 데이터 유효성 검사 강화

--- src/dao/manager/managerFormBidDao.js
+++ src/dao/manager/managerFormBidDao.js
@@ -53,6 +53,62 @@ const managerFormBidDao = {
 
     return result;
   },
+
+  /**
+   * 장례식장 ID를 통한 모든 상조팀장의 견적서 리스트 조회
+   */
+  async getAllManagerFormByFuneralId(funeralId) {
+    const result = await ManagerFormBid.findAll({
+      where: {
+        funeralId: funeralId,
+      },
+      attributes: ['managerFormBidId', 'managerFormCreatedAt', 'bidSubmittedAt', 'bidStatus'],
+    });
+
+    return result;
+  },
+
+  /**
+   * managerFormBidId를 기반으로 managerFormId 조회
+   */
+  async getManagerFormIdByManagerFormBidId(managerFormBidId) {
+    const result = await ManagerFormBid.findOne({
+      where: {
+        managerFormBidId: managerFormBidId,
+      },
+      attributes: ['managerFormId'],
+    });
+
+    return result.managerFormId;
+  },
+
+  /**
+   * managerFormBidId를 기반으로 입찰 신청서 조회
+   */
+  async getManagerFormBidById(managerFormBidId) {
+    const bid = await ManagerFormBid.findByPk(managerFormBidId);
+    return bid;
+  },
+
+  /**
+   * 장례식장 입찰 신청
+   */
+  async updateManagerFormBid(params) {
+    const result = await ManagerFormBid.update(
+      {
+        ...params,
+        bidStatus: 'bid_submitted',
+        bidSubmittedAt: new Date(),
+      },
+      {
+        where: {
+          managerFormBidId: params.managerFormBidId,
+        },
+      },
+    );
+
+    return result;
+  },
 };
 
 export default managerFormBidDao;
